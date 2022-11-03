9dc84174b90895c2beeb80d72159dca040dca9fe
Ahtisham Kiyani <ehtisham.alphasquad@gmail.com>
2022-11-03 22:09:18 +0500

    image grid

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import PuzzleImage from './PuzzleImage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <PuzzleImage/>
   </React.StrictMode>
 );
 
