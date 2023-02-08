6c55a450aeaac527458059bb4cac1a418eb2e7c6
Jamal Khan <ahtishamkayani01@gmail.com>
2023-02-08 18:31:44 +0500

    tsk change

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import PuzzleImage from "./PuzzleImage";
-
+import PuzzleGrid from "./PuzzleGrid";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <PuzzleImage />
+    // <PuzzleImage />
+    // <PuzzleGrid/>
 
-    // <React.StrictMode>
-    //     {/* <App /> */}
-    // </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
